fbb6927f9637aa08dcd0e3237c3245888eb64ec1
17146 <1714673995@qq.com>
2025-09-05 15:12:56 +0800

    调整名称显示

--- src/main/java/com/atsuishio/superbwarfare/client/overlay/KillMessageOverlay.java
+++ src/main/java/com/atsuishio/superbwarfare/client/overlay/KillMessageOverlay.java
@@ -369,19 +369,22 @@ public class KillMessageOverlay implements IGuiOverlay {
 
     public static String getEntityName(Entity entity) {
         String[] name = {entity.getDisplayName().getString()};
-        if (!DisplayConfig.DOG_TAG_NAME_VISIBLE.get()) return name[0];
         if (entity instanceof LivingEntity living && living instanceof OwnableEntity ownableEntity && ownableEntity.getOwner() instanceof Player player) {
-            CuriosApi.getCuriosInventory(player).ifPresent(
-                    c -> c.findFirstCurio(ModItems.DOG_TAG.get()).ifPresent(
-                            s -> {
-                                if (s.stack().hasCustomHoverName()) {
-                                    name[0] = s.stack().getHoverName().getString();
+            if (DisplayConfig.DOG_TAG_NAME_VISIBLE.get()) {
+                CuriosApi.getCuriosInventory(player).ifPresent(
+                        c -> c.findFirstCurio(ModItems.DOG_TAG.get()).ifPresent(
+                                s -> {
+                                    if (s.stack().hasCustomHoverName()) {
+                                        name[0] = s.stack().getHoverName().getString() + " + " + name[0];
+                                    }
                                 }
-                            }
-                    )
-            );
-            name[0] += " + " + entity.getDisplayName().getString();
+                        )
+                );
+            } else {
+                name[0] = player.getDisplayName().getString() + " + " + name[0];
+            }
         } else if (entity instanceof Player player) {
+            if (!DisplayConfig.DOG_TAG_NAME_VISIBLE.get()) return name[0];
             CuriosApi.getCuriosInventory(player).ifPresent(
                     c -> c.findFirstCurio(ModItems.DOG_TAG.get()).ifPresent(
                             s -> {
