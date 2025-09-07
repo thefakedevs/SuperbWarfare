3f3e2734f64115be988dea761a839e97bee18f67
17146 <1714673995@qq.com>
2025-09-07 22:54:29 +0800

    调整集束弹分裂tick判定

--- src/main/java/com/atsuishio/superbwarfare/entity/projectile/CannonShellEntity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/projectile/CannonShellEntity.java
@@ -249,7 +249,7 @@ public class CannonShellEntity extends FastThrowableProjectile implements GeoEnt
             this.discard();
         }
 
-        if (type == Type.CM && tickCount > 1) {
+        if (type == Type.CM && tickCount > 3) {
             // 使用Minecraft内置的光线追踪进行碰撞检测
             BlockHitResult hitResult = level().clip(new ClipContext(
                     position(),
