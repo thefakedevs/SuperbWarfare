5cd92d7e7f43603ab9a9ceaef0ee0f2275bffba8
lavafrai <lavafrai@yandex.ru>
2026-01-20 04:31:22 +0300

    Remove "_mapped_" suffix from dependency versions in POM XML

--- build.gradle.kts
+++ build.gradle.kts
@@ -271,6 +271,22 @@ publishing {
             groupId = project.group.toString()
             artifactId = base.archivesName.get()
             version = project.version.toString()
+
+            pom.withXml {
+                val dependenciesNode = asNode().get("dependencies") as groovy.util.NodeList
+                dependenciesNode.forEach { dependency ->
+                    val depNode = dependency as groovy.util.Node
+                    val versionList = depNode.get("version") as groovy.util.NodeList
+
+                    if (versionList.isNotEmpty()) {
+                        val currentVersion = versionList[0] as String
+                        if (currentVersion.contains("_mapped_")) {
+                            val cleanVersion = currentVersion.substringBefore("_mapped_")
+                            versionList[0] = cleanVersion
+                        }
+                    }
+                }
+            }
         }
 
         create<MavenPublication>("mavenMapped") {
