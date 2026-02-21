43235339da3b04ac79773bc673e010c27e07bcc8
Artem Baechka <kkunfin@gmail.com>
2026-02-21 15:29:26 +0900

    Revert "tow now drops itself"

--- src/main/java/com/atsuishio/superbwarfare/entity/vehicle/TowEntity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/vehicle/TowEntity.java
@@ -173,9 +173,9 @@ public class TowEntity extends GeoVehicleEntity {
             var y = this.getY();
             var z = this.getZ();
             level.explode(null, x, y, z, 0, Level.ExplosionInteraction.NONE);
-            ItemEntity tow = new ItemEntity(level, x, (y + 1), z, new ItemStack(ModItems.TOW_DEPLOYER.get()));
-            tow.setPickUpDelay(10);
-            level.addFreshEntity(tow);
+            ItemEntity mortar = new ItemEntity(level, x, (y + 1), z, new ItemStack(ModItems.MORTAR_BARREL.get()));
+            mortar.setPickUpDelay(10);
+            level.addFreshEntity(mortar);
         }
         super.destroy();
     }
